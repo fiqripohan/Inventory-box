0c4b39380e42588b52fe9f9a4cac188bfa7fc9ab
Fiqri Muhammad Zuhair Pohan <fzuhairpohan@gmail.com>
2021-06-12 03:20:39 +0700

    CRUD inventori dan qrcode

--- application/controllers/panel/Inventori.php
+++ application/controllers/panel/Inventori.php
@@ -160,4 +160,156 @@ class Inventori extends CI_Controller
 		}
 	}
 
+	//------------------------ INVENTORI BEGIN ------------------------//
+	public function listInventori($param1='')
+	{
+		if (cekModul($this->akses_controller) == FALSE) redirect('auth/access_denied');
+		if ($param1=='cari') {
+			$id_kategori = $this->input->post('id_kategori');
+			return $this->InventoriModel->getDataInventori($id_kategori);
+		} else {
+			$data['title'] = $this->title;
+			$data['subtitle'] = 'Daftar Inventori';
+			$data['content'] = 'panel/inventori/index';
+			$data['getKategori'] = $this->GeneralModel->get_general('e_kategori_inventori');
+			$data['id_kategori'] = $this->input->get('id_kategori');
+			$this->load->view('panel/content', $data);
+		}
+	}
+
+	public function createInventori($param1 = '')
+	{
+		if (cekModul($this->akses_controller) == FALSE) redirect('auth/access_denied');
+		if ($param1 == 'doCreate') {
+			$dataInventori = array(
+				'nama_inventori' => $this->input->post('nama_inventori'),
+				'satuan_inventori' => $this->input->post('id_satuan'),
+				'harga_jual' => $this->input->post('harga_jual'),
+				'kategori_inventori' => $this->input->post('id_kategori'),
+				'created_by' => $this->session->userdata('id_pengguna')
+			);
+
+			$tempdir = "assets/img/qrbarang/";
+			if (!file_exists($tempdir))
+				mkdir($tempdir);
+
+			$identitasAplikasi = $this->GeneralModel->get_by_id_general('e_identitas','id_profile',1);
+
+			$logopath = $identitasAplikasi[0]->logo;
+
+			if ($this->GeneralModel->create_general('e_inventori', $dataInventori) == true) {
+				$id_inventori = $this->db->insert_id();
+				
+				//isi qrcode jika di scan
+				$codeContents = base_url('panel/inventori/detailInventori/'). $id_inventori;
+				//simpan file qrcode
+				QRcode::png($codeContents, $tempdir . $dataInventori['nama_inventori'].'.png', QR_ECLEVEL_H, 10, 4);
+
+				// ambil file qrcode
+				$QR = imagecreatefrompng($tempdir .$dataInventori['nama_inventori'] . '.png');
+
+				$dataInventori2 = array(
+					'qrcode' => 'assets/img/qrbarang/'.$dataInventori['nama_inventori'].'.png'
+				);
+
+				$this->GeneralModel->update_general('e_inventori', 'id_inventori', $id_inventori, $dataInventori2);
+
+				// memulai menggambar logo dalam file qrcode
+				$logo = imagecreatefromstring(file_get_contents($logopath));
+
+				imagecolortransparent($logo, imagecolorallocatealpha($logo, 0, 0, 0, 127));
+				imagealphablending($logo, false);
+				imagesavealpha($logo, true);
+
+				$QR_width = imagesx($QR);
+				$QR_height = imagesy($QR);
+
+				$logo_width = imagesx($logo);
+				$logo_height = imagesy($logo);
+
+				// Scale logo to fit in the QR Code
+				$logo_qr_width = $QR_width / 8;
+				$scale = $logo_width / $logo_qr_width;
+				$logo_qr_height = $logo_height / $scale;
+
+				imagecopyresampled($QR, $logo, $QR_width / 2.3, $QR_height / 2.3, 0, 0, $logo_qr_width, $logo_qr_height, $logo_width, $logo_height);
+
+				// Simpan kode QR lagi, dengan logo di atasnya
+				imagepng($QR, $tempdir . $dataInventori['nama_inventori'] . '.png');
+
+				$this->session->set_flashdata('notif', '<div class="alert alert-success">Data inventori berhasil ditambahkan</div>');
+				redirect(changeLink('panel/inventori/listInventori'));
+			} else {
+				$this->session->set_flashdata('notif', '<div class="alert alert-danger">Data inventori gagal ditambahkan</div>');
+				redirect(changeLink('panel/inventori/listInventori'));
+			}
+		} else {
+			$data['title'] = $this->title;
+			$data['subtitle'] = 'Tambah Inventori';
+			$data['content'] = 'panel/inventori/create';
+			$data['kategori'] = $this->GeneralModel->get_general('e_kategori_inventori');
+			$data['satuan'] = $this->GeneralModel->get_general('e_satuan_inventori');
+			$this->load->view('panel/content', $data);
+		}
+	}
+
+	public function updateInventori($param1 = '', $param2 = '')
+	{
+		if (cekModul($this->akses_controller) == FALSE) redirect('auth/access_denied');
+		if ($param1 == 'doUpdate') {
+			$dataInventori = array(
+				'nama_inventori' => $this->input->post('nama_inventori'),
+				'satuan_inventori' => $this->input->post('id_satuan'),
+				'harga_jual' => $this->input->post('harga_jual'),
+				'kategori_inventori' => $this->input->post('id_kategori'),
+				'updated_by' => $this->session->userdata('id_pengguna'),
+				'updated_time' => DATE('Y-m-d H:i:s')
+			);
+			if ($this->GeneralModel->update_general('e_inventori', 'id_inventori', $param2, $dataInventori) == true) {
+				$this->session->set_flashdata('notif', '<div class="alert alert-success">Data inventori berhasil diupdate</div>');
+				redirect(changeLink('panel/inventori/listInventori'));
+			} else {
+				$this->session->set_flashdata('notif', '<div class="alert alert-danger">Data inventori gagal diupdate</div>');
+				redirect(changeLink('panel/inventori/listInventori'));
+			}
+		} else {
+			$data['title'] = $this->title;
+			$data['subtitle'] = 'Update Inventori';
+			$data['content'] = 'panel/inventori/update';
+			$data['inventori'] = $this->GeneralModel->get_by_id_general('e_inventori', 'id_inventori', $param1);
+			$data['satuan'] = $this->GeneralModel->get_general('e_satuan_inventori');
+			$data['kategori'] = $this->GeneralModel->get_general('e_kategori_inventori');
+			$this->load->view('panel/content', $data);
+		}
+	}
+
+	public function deleteInventori($param1 = '')
+	{
+		if (cekModul($this->akses_controller) == FALSE) redirect('auth/access_denied');
+		$inventori = $this->GeneralModel->get_by_id_general('e_inventori', 'id_inventori', $param1);
+		if (!empty($inventori[0]->qrcode)) {
+			try {
+				unlink($inventori[0]->qrcode);
+			} catch (\Exception $e) {
+			}
+		}
+		if ($this->GeneralModel->delete_general('e_inventori', 'id_inventori', $param1) == TRUE) {
+			$this->session->set_flashdata('notif', '<div class="alert alert-success">Data inventori berhasil dihapus</div>');
+			redirect(changeLink('panel/inventori/listInventori'));
+		} else {
+			$this->session->set_flashdata('notif', '<div class="alert alert-danger">Data inventori gagal dihapus</div>');
+			redirect(changeLink('panel/inventori/listInventori'));
+		}
+	}
+
+	public function detailInventori($param1=''){
+		if (cekModul($this->akses_controller) == FALSE) redirect('auth/access_denied');
+		$data['title'] = $this->title;
+		$data['subtitle'] = 'Detail Inventori';
+		$data['content'] = 'panel/inventori/detail';
+		$data['inventori'] = $this->GeneralModel->get_by_id_general('e_inventori', 'id_inventori', $param1);
+		$data['satuan'] = $this->GeneralModel->get_general('e_satuan_inventori');
+		$data['kategori'] = $this->GeneralModel->get_general('e_kategori_inventori');
+		$this->load->view('panel/content', $data);
+	}
 }
---
+++ application/views/panel/inventori/index.php
@@ -0,0 +1,154 @@
+<!-- begin #content -->
+<div id="content" class="content">
+  <!-- begin breadcrumb -->
+  <ol class="breadcrumb pull-right">
+    <li><a href="javascript:;">Home</a></li>
+    <li><a href="javascript:;"><?php echo $title; ?></a></li>
+    <li class="active"><?php echo $subtitle; ?></li>
+  </ol>
+  <!-- end breadcrumb -->
+  <!-- begin page-header -->
+  <h1 class="page-header"><?php echo $title; ?></h1>
+  <!-- end page-header -->
+
+  <!-- begin row -->
+  <div class="row">
+    <!-- begin col-12 -->
+    <div class="col-md-12">
+      <!-- begin panel -->
+      <div class="panel panel-inverse">
+        <div class="panel-heading">
+          <div class="panel-heading-btn">
+            <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-default" data-click="panel-expand"><i class="fa fa-expand"></i></a>
+            <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-success" data-click="panel-reload"><i class="fa fa-repeat"></i></a>
+            <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-warning" data-click="panel-collapse"><i class="fa fa-minus"></i></a>
+            <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-danger" data-click="panel-remove"><i class="fa fa-times"></i></a>
+          </div>
+          <h4 class="panel-title"><?php echo $subtitle; ?></h4>
+        </div>
+        <div class="panel-body">
+          <?php echo $this->session->flashdata('notifpass'); ?>
+          <?php echo $this->session->flashdata('notif'); ?>
+          <div class="col-md-4">
+            <select class="form-control select2" id="id_kategori" onchange="carikategori(this.value)">
+              <option value="">.:Pilih Kategori Inventori:.</option>
+              <?php foreach ($getKategori as $key) : ?>
+                <option value="<?php echo $key->id_kategori; ?>"><?php echo $key->nama_kategori; ?></option>
+              <?php endforeach; ?>
+            </select>
+          </div>
+          <script type="text/javascript">
+            $('#id_kategori').val('<?php echo $id_kategori; ?>')
+
+            function cariKategori(val) {
+              location.replace('<?php echo base_url(changeLink('panel/inventori/listInventori?id_kategori=')); ?>' + val)
+            }
+          </script>
+          <a href="<?php echo base_url(changeLink('panel/inventori/createInventori/')); ?>" class="btn btn-xs btn-primary pull-right">Tambah Inventori</a>
+          <br />
+          <br />
+          <br />
+          <table id="table" class="table table-striped table-bordered" width="100%">
+            <thead>
+              <tr>
+                <th>NO</th>
+                <th>Nama Barang</th>
+                <th>Kategori</th>
+                <th>Jumlah</th>
+                <th>Harga Pokok</th>
+                <th>Harga Jual</th>
+                <th>QR CODE</th>
+                <th>Aksi</th>
+              </tr>
+            </thead>
+            <tbody>
+            </tbody>
+          </table>
+          <?php echo $this->session->flashdata('notif'); ?>
+          <?php echo $this->session->flashdata('notifpass'); ?>
+        </div>
+      </div>
+      <!-- end panel -->
+    </div>
+    <!-- end col-12 -->
+  </div>
+  <!-- end row -->
+</div>
+<!-- end #content -->
+<script type="text/javascript">
+  var table;
+
+  $(document).ready(function() {
+    table = $('#table').DataTable({
+      responsive: {
+        breakpoints: [{
+          name: 'not-desktop',
+          width: Infinity
+        }]
+      },
+      "filter": true,
+      "processing": true, //Feature control the processing indicator.
+      "serverSide": true, //Feature control DataTables' server-side processing mode.
+      "order": [], //Initial no order.
+      "lengthChange": true,
+      // Load data for the table's content from an Ajax source
+      "ajax": {
+        "url": '<?php echo site_url(changeLink('panel/inventori/listInventori/cari')); ?>',
+        "type": "POST",
+        "data": {
+          "id_kategori": "<?php echo $id_kategori; ?>"
+        }
+      },
+      //Set column definition initialisation properties.
+      "columns": [{
+          "data": null,
+          width: 10,
+          "sortable": false,
+          render: function(data, type, row, meta) {
+            return meta.row + meta.settings._iDisplayStart + 1;
+          }
+        },
+        {
+          "data": "nama_inventori",
+          width: 100,
+        },
+        {
+          "data": "nama_kategori",
+          width: 100
+        },
+        {
+          "data": "jumlah_inventori",
+          width: 100,
+          render: function(data, type, row, meta) {
+            return new Intl.NumberFormat().format(row.jumlah_inventori) + ' ' + row.singkatan_satuan
+          }
+        },
+        {
+          "data": "harga_pokok",
+          width: 100,
+          render: function(data, type, row, meta) {
+            return "Rp" + new Intl.NumberFormat().format(row.harga_pokok)
+          }
+        },
+        {
+          "data": "harga_jual",
+          width: 100,
+          render: function(data, type, row, meta) {
+            return "Rp" + new Intl.NumberFormat().format(row.harga_jual)
+          }
+        },
+        {
+          "data": "qrcode",
+          width: 100,
+          render: function(data, type, row, meta) {
+            return "<img src='<?php echo base_url(); ?>" + row.qrcode + "' class='img-responsive' style='width:250px'>"
+          }
+        },
+        {
+          "data": "action",
+          width: 100
+        },
+      ],
+    });
+  });
+</script>
